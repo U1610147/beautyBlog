9b87027f9f6d1082cc39e1244d1475001e707425
U1610147 <m.rakhmonova@student.inha.uz>
2018-05-02 14:02:12 +0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+<h1>About</h1>
+@section('content')
+
+<h1>About</h1>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <h1>Beauty blog</h1>
+        <br>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Fashion tips</a></li>
+      <li><a href="#">Contact us</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+</div>
+<img class="i1" src="interns-facebook.jpg">
+    </body>
+</html>
+
